f698dccb5cb851e809d432430ca66f6c176bd27e
antrophus <antrophus@yahoo.com>
2025-01-24 14:29:34 +0900

    remove server folder

--- server/scripts/kill-server.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Killing server process on port 4000...
-for /f "tokens=5" %%a in ('netstat -aon ^| find ":4000" ^| find "LISTENING"') do taskkill /f /pid %%a
-echo Server process terminated. 
